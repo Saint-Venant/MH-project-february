08554962a2cad265feb9c97d99750eb6056117bc
Matt <matthieu.roux92@gmail.com>
2020-02-22 12:12:11 +0100

    Adapt output structure for the new cutting planes algorithm

--- Parse_instance/readOutput.py
+++ Parse_instance/readOutput.py
@@ -2,63 +2,109 @@ import numpy as np
 
 
 class OutputFile:
-    def __init__(self, outputFileName='output.dat', outputDir='./'):
+    def __init__(self, algo, outputFileName='output.dat', outputDir='./'):
+        assert(algo in ['branch_and_bound_OPL', 'cutting_planes'])
+        
         self.fileName = outputFileName
         self.file = outputDir + outputFileName
-        
+        self.algo = algo
+
         self.status = -1
-        self.gap = -1
-        self.bestInteger = -1
         self.infBound = -1
         self.select = -1
         self.solvingTime = -1
-        self.loadData()
+        if self.algo == 'branch_and_bound_OPL':
+            self.gap = -1
+            self.bestInteger = -1
+            self.loadData_OPL()
+        elif self.algo == 'cutting_planes':
+            self.nbContinuousIt = -1
+            self.nbIntegerIt = -1
+            self.sequenceLowBounds = -1
+            self.loadData_cuttingPlanes()
 
-    def loadData(self):
+    def loadData_OPL(self):
         with open(self.file, 'r') as f:
             content = f.readlines()
-            self.status = int(content[0].split(' = ')[1][:-2])
-            self.gap = float(content[2].split(' = ')[1][:-2])
-            self.bestInteger = int(content[4].split(' = ')[1][:-2])
-            self.infBound = float(content[6].split(' = ')[1][:-2])
+        self.status = int(content[0].split(' = ')[1][:-2])
+        self.gap = float(content[2].split(' = ')[1][:-2])
+        self.bestInteger = int(content[4].split(' = ')[1][:-2])
+        self.infBound = float(content[6].split(' = ')[1][:-2])
 
-            select = []
-            row = 8
-            for elem in content[row].split(' =  ')[1].split(' '):
-                if elem[0] == '[':
-                    select.append(int(elem[1:]))
-                elif elem[-1] == '\n':
+        select = []
+        row = 8
+        for elem in content[row].split(' =  ')[1].split(' '):
+            if elem[0] == '[':
+                select.append(int(elem[1:]))
+            elif elem[-1] == '\n':
+                select.append(int(elem[:-1]))
+            else:
+                select.append(int(elem))
+        row += 1
+        while (len(content[row]) > 1):
+            for elem in content[row].split(' '):
+                if elem == '':
+                    pass
+                elif ']' in elem:
+                    select.append(int(elem[:-3]))
+                elif '\n' in elem:
                     select.append(int(elem[:-1]))
                 else:
                     select.append(int(elem))
             row += 1
-            while (len(content[row]) > 1):
-                for elem in content[row].split(' '):
-                    if elem == '':
-                        pass
-                    elif ']' in elem:
-                        select.append(int(elem[:-3]))
-                    elif '\n' in elem:
-                        select.append(int(elem[:-1]))
-                    else:
-                        select.append(int(elem))
-                row += 1
-            self.select = select
+        self.select = select
 
-            row += 1
-            self.solvingTime = int(content[row].split(' = ')[1][:-2])
+        row += 1
+        self.solvingTime = int(content[row].split(' = ')[1][:-2])
 
+    def loadData_cuttingPlanes(self):
+        with open(self.file, 'r') as f:
+            content = f.readlines()
+        self.status = content[0].split(' = ')[1][:-2]
+        self.infBound = float(content[2].split(' = ')[1][:-2])
+        self.nbContinuousIt = int(content[4].split(' = ')[1][:-2])
+        self.nbIntegerIt = int(content[6].split(' = ')[1][:-2])
+        select = []
+        for elem in content[8].split(' = ')[1].split(' '):
+            if elem[0] == '[':
+                select.append(int(elem[1:]))
+            elif ']' in elem:
+                select.append(int(elem[:-3]))
+            else:
+                select.append(int(elem))
+        self.select = select
+        sequence = []
+        for elem in content[10].split(' = ')[1].split(' '):
+            if elem[0] == '{':
+                sequence.append(float(elem[1:-1]))
+            elif '}' in elem:
+                sequence.append(float(elem[:-3]))
+            else:
+                sequence.append(float(elem[:-1]))
+        self.sequenceLowBounds = sequence
+        self.solvingTime = int(content[12].split(' = ')[1][:-2])
+
+    def displayContent(self):
+        print('status = ', self.status, '\n')
+        print('inf bound = ', self.infBound, '\n')
+        if self.algo == 'branch_and_bound_OPL':
+            print('gap = ', self.gap, '\n')
+            print('best integer = ', self.bestInteger, '\n')
+        elif self.algo == 'cutting_planes':
+            print('nb continuous iterations = ', self.nbContinuousIt, '\n')
+            print('nb integer iterations = ', self.nbIntegerIt, '\n')
+            print('sequence of lower bounds = ', self.sequenceLowBounds, '\n')
+        print('nb nodes = ', len(self.select), '\n')
+        print('select = ', self.select, '\n')
+        print('solving time = ', self.solvingTime)
 
 
 
 
 if __name__ == '__main__':
-    output = OutputFile()
-
-    print('status = ', output.status, '\n')
-    print('gap = ', output.gap, '\n')
-    print('best integer = ', output.bestInteger, '\n')
-    print('inf bound = ', output.infBound, '\n')
-    print('select = ', output.select, '\n')
-    print('nb nodes = ', len(output.select), '\n')
-    print('solving time = ', output.solvingTime, '\n')
+    #algo = 'cutting_planes'
+    algo = 'branch_and_bound_OPL'
+    
+    output = OutputFile(algo)
+    output.displayContent()
+    
